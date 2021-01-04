ed1202163de5fb73a50e485e2a1d2001dc70c18b
fletcherjiang <fletcherjang@gmail.com>
2021-01-04 17:03:23 +0800

    modify

--- toolchain/profiling_manager.cpp
+++ toolchain/profiling_manager.cpp
@@ -158,7 +158,7 @@ aclError AclProfilingManager::QueryHashValue(const char *funcName, int &deviceId
     return ACL_SUCCESS;
 }
 
-AclProfilingReporter::AclProfilingReporter(const char *funcName, ProfFuncType funcType) : funcName_(funcName), funcType_(funcType)
+AclProfilingReporter::AclProfilingReporter(const char *funcName, ProfFuncType funcType) : funcType_(funcType), funcName_(funcName)
 {
     if (AclProfilingManager::GetInstance().AclProfilingIsRun()) {
         if (aclrtGetDevice(&deviceId_) != ACL_SUCCESS) {
